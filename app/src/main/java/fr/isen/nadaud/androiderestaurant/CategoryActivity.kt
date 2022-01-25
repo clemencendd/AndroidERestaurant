fcd49c4ab5744e726da2561a1c718fe44c859cfb
Nadaud <clemence.nadaud@isen.yncrea.fr>
2022-01-25 10:48:24 +0100

    first commit

---
+++ app/src/main/java/fr/isen/nadaud/androiderestaurant/CategoryActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.nadaud.androiderestaurant
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category)
+    }
+}
